3c38daf0bf4fe24b93c0b2a6bd0002d4f6f4e50f
Edwin Babu <edwin@Edwins-MacBook-Pro.local>
2020-07-12 17:47:13 +0530

    env support added

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+require("dotenv-extended").load();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+console.log('env:', process.env.PORT)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
