315a6d60c77bed6ebc19e5f231919d91c91f3be9
Edwin Babu <edwinbbu@gmail.com>
2020-07-13 16:22:05 +0530

    tailwindcss loaded

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../styles/main.scss';
 
 const App = ()=>{
-  return (<>
+  return (<div>
     <h1>Welcome to React</h1>
-  </>
+    <p class="font-sans text-lg text-gray-800 text-center">
+      I'm a sans-serif paragraph.
+    </p>
+  </div>
   );
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const domain = process.env.ENV === 'development' ? process.env.CLIENT_URL : '';
+  const domain = process.env.ENV === 'development' ? process.env.DEV_URL : '';
   res.render('index', { title: 'Express', domain });
 });
 
