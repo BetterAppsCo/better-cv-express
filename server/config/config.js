09eecfc681dc36bc9b67eff627a2ccdda5c42caa
Edwin Babu <edwinbbu@gmail.com>
2020-08-01 10:27:42 +0530

    migrations added and user table created

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: 'postgres',
+    password: '',
+    database: 'bettercv',
     host: process.env.DB_HOST,
     dialect: 'postgres'
   },
--- server/models/register.js
+++ server/models/register.js
@@ -1,10 +1,9 @@
 const hooksCollection = require('models/hooks');
-// const instanceMethodsCollection = require('models/instanceMethods');
-// const staticMethodsCollection = require('models/staticMethods');
 
 /** @funtion register
  *  Register hooks, instanceMethods and staticMethods to the models
  */
+
 module.exports = () => {
   //Register Hooks
   for (const key of Object.keys(hooksCollection)) {
@@ -13,18 +12,4 @@ module.exports = () => {
       metaData.model['addHook'](lifeCycleName, metaData.hooks[lifeCycleName]);
     } 
   }
-  // //Register instance methods
-  // for (const key of Object.keys(instanceMethodsCollection)) {
-  //   const metaData = instanceMethodsCollection[key];
-  //   for (const methodName of Object.keys(metaData.instanceMethods)) {
-  //     metaData.model['prototype'][methodName] = metaData.instanceMethods[methodName];
-  //   } 
-  // }
-  // //Register static methods
-  // for (const key of Object.keys(staticMethodsCollection)) {
-  //   const metaData = staticMethodsCollection[key];
-  //   for (const methodName of Object.keys(metaData.staticMethods)) {
-  //     metaData.model[methodName] = metaData.staticMethods[methodName];
-  //   }
-  // }
 };
